1e301528bc528231933755e718a3a276baa7807e
Iyonissio <iyonissio01@gmail.com>
2021-02-05 10:03:59 -0800

    Add new

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     padding: 80px 60px;
-    background: radial-gradient(circle, rgba(92, 39, 261, 1) 0%, rgba(112,71,247,1) 100%);
+    background: radial-gradient(circle, rgba(92, 40, 261, 1) 0%, rgba(112,71,247,1) 100%);
 `
 
 export const Wrapper = styled.div`
